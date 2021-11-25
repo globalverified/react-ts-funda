86f7c8448b241ab425f853953e8e88dc96a23c93
sagar-github-2021 <sagar8653170300@gmail.com>
2021-11-25 12:59:24 +0530

    removed extra code

--- src/App.tsx
+++ src/App.tsx
@@ -67,14 +67,6 @@ function App() {
         <br />-----------<br />
         <UseContextHook />
         <br />-----------<br />
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
